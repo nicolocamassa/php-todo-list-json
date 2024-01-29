ff2500f7b2d9bae35ecf9ea208836214172ed9a5
nicolocamassa <camassanicolo03@gmail.com>
2024-01-29 17:39:26 +0100

    [UPDATE] keep data from file.json

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-
+            apiEndpoint: 'server.php',
+            todoList: [],
         }
-    },
+    }, mounted(){
+        this.importTodo();
+    }, methods: {
+        importTodo(){
+            axios.get(this.apiEndpoint).then((response) => {
+                this.todoList = response.data;
+            })
+        }
+    }
 }).mount('#app')
--- server.php
+++ server.php
@@ -1,3 +1,7 @@
 <?php 
+    $jsonFile = file_get_contents('file.json');
 
+    header('contet-type: application/json');
+
+    echo $jsonFile;
 ?>
